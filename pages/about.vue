86473317a7f7f72b74ac226259abe86c09912b60
NTTPS <teamerze@gmail.com>
2024-01-05 13:07:10 +0700

    fix

--- pages/about.vue
+++ pages/about.vue
@@ -1,88 +1,90 @@
 <template>
-    <div class="max-w-screen-2xl w-full mx-auto mt-8 pb-4">
-        <div class="flex justify-between mb-8 bg-zinc-100">
+    <div>
+        <div class="max-w-screen-2xl w-full mx-auto mt-8 pb-4">
+            <div class="flex justify-between mb-8 bg-zinc-100">
 
-            <div class="h-[185px] w-[196px] bg-[#2a363b]">
-                <img src="~/assets/images/about-ckc.png" class="object-contain h-[185px] w-[196px] " alt="">
+                <div class="h-[185px] w-[196px] bg-[#2a363b]">
+                    <img src="~/assets/images/about-ckc.png" class="object-contain h-[185px] w-[196px] " alt="">
+                </div>
+                <div class="flex flex-col justify-between items-end">
+                    <div class="text-5xl">Why Choose CKC Consulting?</div>
+                    <div class="text-2xl font-bold">About  CKC</div>
+
+                </div>
             </div>
-            <div class="flex flex-col justify-between items-end">
-                <div class="text-5xl">Why Choose CKC Consulting?</div>
-                <div class="text-2xl font-bold">About  CKC</div>
-
-            </div>
-        </div>
-        <div>
-            <span class="text-2xl">​Founded in 2020</span>, it is a company that provides consulting services, technical solutions, and processes for setting up systems, managing, supervising, analyzing, and evaluating risks. and issue a summary report Along with providing convenience to customers by both leasing and leasing equipment services with maintenance along with selecting products with modern technology suitable and quality to suit the needs of the organization to support future system expansion Easy to manage under a reasonable budget and has cooperated with agencies and business partners to work together to conduct business in all aspects for maximum efficiency which will bring maximum customer satisfaction.
-        </div>
-    </div>
-
-    <div class="max-w-screen-xl w-full mx-auto mt-8 pb-4">
-        <div class="flex items-center space-x-8 border-b-8 border-[#F8B500] pb-4">
-            <div class="text-6xl font-bold">Vision</div>
-            <div class="bg-[#F8B500] italic p-5 text-xl">
-                CyberKings is dedicated to growing steadily towards becoming a leading organization with comprehensive products and state-of-the-art technology, including professional consulting and top-notch services for the utmost satisfaction of our customers.
+            <div>
+                <span class="text-2xl">​Founded in 2020</span>, it is a company that provides consulting services, technical solutions, and processes for setting up systems, managing, supervising, analyzing, and evaluating risks. and issue a summary report Along with providing convenience to customers by both leasing and leasing equipment services with maintenance along with selecting products with modern technology suitable and quality to suit the needs of the organization to support future system expansion Easy to manage under a reasonable budget and has cooperated with agencies and business partners to work together to conduct business in all aspects for maximum efficiency which will bring maximum customer satisfaction.
             </div>
         </div>
-        <div class="flex items-center space-x-8 border-t-8 border-[#2a363b] my-8">
-            <div class="bg-[#2a363b] text-white pt-1 pb-2">
-                <ol class="list-decimal  ml-8">
-                    <li>Supporting creative activities within the organization to exchange knowledge, perspectives, and collaborative abilities (Shared value space).</li>
-                    <li>Recruiting and developing personnel to be experts and professionals.</li>
-                    <li>Engaging and collaborating with business partners to work together in operating businesses comprehensively in all aspects.</li>
-                    <li>Providing tailored consultations that genuinely meet the organizational needs of customers and delivering services to maintain customer loyalty.</li>
-                    <li>Presenting innovative and beneficial solutions in line with the specific requirements of the customer's organization.</li>
-                </ol>
-            </div>
 
-            <div class="text-6xl font-bold">Mission</div>
+        <div class="max-w-screen-xl w-full mx-auto mt-8 pb-4">
+            <div class="flex items-center space-x-8 border-b-8 border-[#F8B500] pb-4">
+                <div class="text-6xl font-bold">Vision</div>
+                <div class="bg-[#F8B500] italic p-5 text-xl">
+                    CyberKings is dedicated to growing steadily towards becoming a leading organization with comprehensive products and state-of-the-art technology, including professional consulting and top-notch services for the utmost satisfaction of our customers.
+                </div>
+            </div>
+            <div class="flex items-center space-x-8 border-t-8 border-[#2a363b] my-8">
+                <div class="bg-[#2a363b] text-white pt-1 pb-2">
+                    <ol class="list-decimal  ml-8">
+                        <li>Supporting creative activities within the organization to exchange knowledge, perspectives, and collaborative abilities (Shared value space).</li>
+                        <li>Recruiting and developing personnel to be experts and professionals.</li>
+                        <li>Engaging and collaborating with business partners to work together in operating businesses comprehensively in all aspects.</li>
+                        <li>Providing tailored consultations that genuinely meet the organizational needs of customers and delivering services to maintain customer loyalty.</li>
+                        <li>Presenting innovative and beneficial solutions in line with the specific requirements of the customer's organization.</li>
+                    </ol>
+                </div>
+
+                <div class="text-6xl font-bold">Mission</div>
+                
+            </div>
+            <div class="text-6xl font-bold text-center mb-4"><h4 class="inline-block border-b-8 border-[#2a363b]  pb-4">Core Value</h4></div>
+            <div class="flex flex-col justify-center text-2xl mx-auto max-w-4xl mb-4">
             
-        </div>
-        <div class="text-6xl font-bold text-center mb-4"><h4 class="inline-block border-b-8 border-[#2a363b]  pb-4">Core Value</h4></div>
-        <div class="flex flex-col justify-center text-2xl mx-auto max-w-4xl mb-4">
-           
-            <div class="flex items-center space-x-4">
-                <div class="text-5xl font-bold">Y</div> 
-                <div>You = You are an important customer.</div>
-            </div>
+                <div class="flex items-center space-x-4">
+                    <div class="text-5xl font-bold">Y</div> 
+                    <div>You = You are an important customer.</div>
+                </div>
 
-            <div class="flex items-center space-x-4">
-                <span class="text-5xl font-bold">B</span> 
-                <div>Build Best Solution = Offering the best solutions to customers.</div>
-            </div>
+                <div class="flex items-center space-x-4">
+                    <span class="text-5xl font-bold">B</span> 
+                    <div>Build Best Solution = Offering the best solutions to customers.</div>
+                </div>
 
-            <div class="flex items-center space-x-4">
-                <span class="text-5xl font-bold">E</span> 
-                <div>Excellenge Team work = Working as a quality and reliable team at all times.</div>
-            </div>
+                <div class="flex items-center space-x-4">
+                    <span class="text-5xl font-bold">E</span> 
+                    <div>Excellenge Team work = Working as a quality and reliable team at all times.</div>
+                </div>
 
-            <div class="flex items-center space-x-4">
-                <span class="text-5xl font-bold">R</span> 
-                <div>Responsibility = Taking responsibility for tasks and duties at every stage of the work process.</div>
-            </div>
+                <div class="flex items-center space-x-4">
+                    <span class="text-5xl font-bold">R</span> 
+                    <div>Responsibility = Taking responsibility for tasks and duties at every stage of the work process.</div>
+                </div>
 
-            <div class="flex items-center space-x-4">
-                <span class="text-5xl font-bold">K</span> 
-                <div>Keen = Enthusiastic, daring to think, and proactive.</div>
-            </div>
-            
-            <div class="flex items-center space-x-4">
-                <span class="text-5xl font-bold">I</span> 
-                <div>Integration & Innovation = Integrating technology and fostering the creation of new innovations.</div>
-            </div>
-            
+                <div class="flex items-center space-x-4">
+                    <span class="text-5xl font-bold">K</span> 
+                    <div>Keen = Enthusiastic, daring to think, and proactive.</div>
+                </div>
+                
+                <div class="flex items-center space-x-4">
+                    <span class="text-5xl font-bold">I</span> 
+                    <div>Integration & Innovation = Integrating technology and fostering the creation of new innovations.</div>
+                </div>
+                
 
-            <div class="flex items-center space-x-4">
-                <span class="text-5xl font-bold">N</span> 
-                <div>New Idea = Innovating new things.</div>
-            </div>
+                <div class="flex items-center space-x-4">
+                    <span class="text-5xl font-bold">N</span> 
+                    <div>New Idea = Innovating new things.</div>
+                </div>
 
-            <div class="flex items-center space-x-4">
-                <span class="text-5xl font-bold">G</span> 
-                <div>Growth Mindset = Unlimited development.</div>
-            </div>
-            <div class="flex items-center space-x-4">
-                <span class="text-5xl font-bold">S</span> 
-                <div>Strategy = Strategically aiming for the goal.</div>
+                <div class="flex items-center space-x-4">
+                    <span class="text-5xl font-bold">G</span> 
+                    <div>Growth Mindset = Unlimited development.</div>
+                </div>
+                <div class="flex items-center space-x-4">
+                    <span class="text-5xl font-bold">S</span> 
+                    <div>Strategy = Strategically aiming for the goal.</div>
+                </div>
             </div>
         </div>
     </div>
