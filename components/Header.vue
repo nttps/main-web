f017c36a3767c80d0a0287d06df615ce38eddbf3
NTTPS <teamerze@gmail.com>
2024-01-10 00:51:28 +0700

    update header

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="px-4 lg:px-0 lg:mx-8 mb-6">
-        <div class="flex flex-wrap xl:flex-nowrap justify-between md:max-w-screen-xl mx-auto w-full pb-2 pt-2 border-b border-black ">
-            <NuxtLink to="/" class="flex items-end">
-                <img src="~assets/images/logo.png" class="h-[100px]" />
-                <span style="letter-spacing:normal;" class="text-4xl font-bold  leading-[1.5rem]">
-                    <span style="color:#2A363B;" class="inline-block">CYBER</span>
-                    <span class="text-[#F8B500] inline-block">KINGS</span>
+        <div class="flex flex-wrap xl:flex-nowrap justify-center md:justify-between md:max-w-screen-xl mx-auto w-full pb-2 pt-2 border-b border-black ">
+            <NuxtLink to="/" class="md:flex items-end">
+                <img src="~assets/images/logo.png" class="h-[100px] mx-auto mb-2 md:mb-0" />
+                <span style="letter-spacing:normal;" class="text-4xl font-bold leading-4 md:leading-[1.7rem]">
+                    <span style="color:#2A363B;" class="">CYBER</span>
+                    <span class="text-[#F8B500] ">KINGS</span>
                 </span>
             </NuxtLink>
-            <div class="md:self-end mx-auto md:mx-0 mt-4 lg:my-4 md:my-0">
+            <div class="sm:self-end mx-auto md:mx-0 mt-4 ">
                 <ul class="flex space-x-4">
                     <li>
                         <NuxtLink to="/" target="_self" exact-active-class="bg-zinc-700 text-white" class="p-1 px-4 bg-[#F8B500] hover:bg-zinc-700 hover:text-white transition-all delay-75 rounded-lg block">Home</NuxtLink>
